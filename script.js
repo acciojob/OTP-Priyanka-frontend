9837743cd31f3dba8b5d869dc9b7d1510953aa9a
Priyanka-frontend <kaushikpriyanka005@gmail.com>
2026-06-13 16:20:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener('input', (e) => {
+    code.value = e.target.value.replace(/[^0-9]/g, '');
+
+    if (code.value && idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
+  });
+
+  code.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (code.value === '' && idx > 0) {
+        codes[idx - 1].value = '';
+        codes[idx - 1].focus();
+      } else {
+        code.value = '';
+      }
+    }
+  });
+});
